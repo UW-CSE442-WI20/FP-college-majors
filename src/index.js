18a97d439274b5b6b760962e551a858dcf6a3e17
Erica Eaton <eatone3@cs.washington.edu>
2020-02-22 17:52:46 -0800

    added code to call functions to create charts when selections change

--- src/index.js
+++ src/index.js
@@ -27,3 +27,99 @@ document.getElementById("backButton").onclick = () => {
   document.getElementById("categories").classList.remove("hidden");
 }
 
+let factorsForCategories = document.getElementById("factorsForCategories");
+updateCategoriesChart(factorsForCategories.value);
+factorsForCategories.onchange = () => {
+  updateCategoriesChart(factorsForCategories.value);
+}
+
+let factorsForTopFive = document.getElementById("factorsForTopFive");
+let categoriesForTopFive = document.getElementById("categoriesForTopFive");
+factorsForTopFive.onchange = () => {
+  updateTopFiveChart(factorsForTopFive.value, categoriesForTopFive.value);
+}
+categoriesForTopFive.onchange = () => {
+  updateTopFiveChart(factorsForTopFive.value, categoriesForTopFive.value);
+}
+
+let majorsForChooseMajors = document.getElementById("majorsForChooseMajors");
+let factorsForChooseMajors = document.getElementById("factorsForChooseMajors");
+let majorOptions = document.querySelectorAll("#majorsForChooseMajors option");
+updateChooseMajorChart(factorsForChooseMajors.value, []);
+majorsForChooseMajors.onchange = () => {
+  var selectedOptions = [];
+  for (var i = 0; i < majorOptions.length; i++) {
+    if (majorOptions[i].selected) {
+      selectedOptions.push(majorOptions[i].value);
+    }
+  }
+  updateChooseMajorChart(factorsForChooseMajors.value, selectedOptions);
+}
+factorsForChooseMajors.onchange = () => {
+  var selectedOptions = [];
+  for (var i = 0; i < majorOptions.length; i++) {
+    if (majorOptions[i].selected) {
+      selectedOptions.push(majorOptions[i].value);
+    }
+  }
+  updateChooseMajorChart(factorsForChooseMajors.value, selectedOptions);
+}
+
+let majorsForSelectFactors = document.getElementById("majorsForSelectFactors");
+let xFactor = document.getElementById("xFactor");
+let yFactor = document.getElementById("yFactor");
+updateChooseFactorsChart([], xFactor.value, yFactor.value);
+majorsForSelectFactors.onchange = () => {
+  var selectedOptions = [];
+  for (var i = 0; i < majorOptions.length; i++) {
+    if (majorOptions[i].selected) {
+      selectedOptions.push(majorOptions[i].value);
+    }
+  }
+  updateChooseFactorsChart(selectedOptions, xFactor.value, yFactor.value);
+}
+xFactor.onchange = () => {
+  var selectedOptions = [];
+  for (var i = 0; i < majorOptions.length; i++) {
+    if (majorOptions[i].selected) {
+      selectedOptions.push(majorOptions[i].value);
+    }
+  }
+  updateChooseFactorsChart(selectedOptions, xFactor.value, yFactor.value);
+}
+yFactor.onchange = () => {
+  var selectedOptions = [];
+  for (var i = 0; i < majorOptions.length; i++) {
+    if (majorOptions[i].selected) {
+      selectedOptions.push(majorOptions[i].value);
+    }
+  }
+  updateChooseFactorsChart(selectedOptions, xFactor.value, yFactor.value);
+}
+
+function updateCategoriesChart(factor) {
+  // TODO
+}
+
+function updateTopFiveChart(factor, category) {
+  // TODO
+}
+
+function updateChooseMajorChart(factor, majors) {
+  if (majors.length > 5) {
+    var chartDiv = document.getElementById("majorsForChooseMajors");
+    chartDiv.innerHTML = "<p>Please select no more than 5 majors to display at once.</p>" + chartDiv.innerHTML;
+  }
+
+  // TODO
+}
+
+function updateChooseFactorsChart(majors, factorXAxis, factorYAxis) {
+  if (majors.length > 5) {
+    var chartDiv = document.getElementById("majorsForSelectFactors");
+    chartDiv.innerHTML = "<p>Please select no more than 5 majors to display at once.</p>" + chartDiv.innerHTML;
+  }
+
+  // TODO
+}
+
