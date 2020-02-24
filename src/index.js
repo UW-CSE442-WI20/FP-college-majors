64ef7f1f6af6730dbb1958667d8442ee03a27d59
Erica Eaton <eatone3@cs.washington.edu>
2020-02-23 16:12:11 -0800

    created bar chart for major categories

--- src/index.js
+++ src/index.js
@@ -98,7 +98,92 @@ yFactor.onchange = () => {
 }
 
 function updateCategoriesChart(factor) {
-  // TODO
+  const data = {
+    "Business": {
+      "Median": 80000,
+      "Unemployment_rate": 2,
+      "ShareWomen": 40
+    },
+    "Engineering": {
+      "Median": 100000,
+      "Unemployment_rate": 1,
+      "ShareWomen": 35
+    },
+    "Science": {
+      "Median": 70000,
+      "Unemployment_rate": 3,
+      "ShareWomen": 39
+    }
+  };
+
+  const majorCategories = ["Business", "Engineering", "Science"];
+
+  var maxValue = 1;
+  for (var i = 0; i < majorCategories.length; i++) {
+    maxValue = Math.max(maxValue, data[majorCategories[i]][factor]);
+  }
+
+  var categoriesChartDiv = document.getElementById("categoriesChart");
+  categoriesChartDiv.style.width = "45rem";
+  categoriesChartDiv.style.height = "30rem";
+  var w = window.getComputedStyle(categoriesChartDiv).width;
+  var h = window.getComputedStyle(categoriesChartDiv).height;
+  w = w.substring(0, w.length - 2);
+  h = h.substring(0, h.length - 2);
+  const padding = w * 0.15;
+
+  const xScale = d3
+    .scaleLinear()
+    .domain([0, maxValue])
+    .range([padding, w - padding]);
+
+  const yScale = d3
+    .scaleBand()
+    .domain(majorCategories)
+    .range([h - padding, padding])
+    .paddingInner(0.2)
+    .paddingOuter(0.2);
+
+  const xAxis = d3.axisBottom().scale(xScale).tickSizeOuter(0);
+  const yAxis = d3.axisLeft().scale(yScale).tickSizeOuter(0);
+  const svg = d3
+    .select("#categoriesChart")
+    .select("svg");
+
+  svg
+    .selectAll("rect")
+    .data(majorCategories)
+    .enter()
+    .append("rect")
+    .attr("x", function (d) {
+      return xScale(0);
+    })
+    .attr("y", function (d) {
+      return yScale(d) + yScale.bandwidth() / 4;
+    })
+    .attr("height", yScale.bandwidth() / 2)
+    .attr("width", function (d) {
+      return xScale(data[d][factor]) - xScale(0);
+    })
+    .attr("fill", "blue")
+    .on("click", function(d) {
+      document.getElementById("categories").classList.add("hidden");
+      document.getElementById("top5").classList.remove("hidden");
+      updateTopFiveChart(factor, d);
+    });
+
+  svg
+    .append("g")
+    .attr("id", "x-axis")
+    .attr("transform", "translate(0," + (h - padding) + ")")
+    .call(xAxis);
+
+  svg
+    .append("g")
+    .attr("id", "y-axis")
+    .attr("transform", "translate(" + padding + ",0)")
+    .call(yAxis);
+
 }
 
 function updateTopFiveChart(factor, category) {
