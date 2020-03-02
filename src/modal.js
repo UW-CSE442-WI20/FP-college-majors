be8770c869e71cac91506aab0e192e458b9214c0
jorycw <jorycw@cs.washington.edu>
2020-03-02 02:08:54 -0800

    added optgroups

--- src/modal.js
+++ src/modal.js
@@ -2,7 +2,8 @@ function updateGraph() {
   // call what you need in here, pass in "selected", which will give you an array of majors
   // i.e. ["Aerospace Engineering", "Materials Science"]
   // update graph will be called whenever user selects different majors in modal and saves changes
-  alert(selected + "have been selected");
+  
+  // alert(selected + "have been selected");
 }
 
 function updateModal(majors) {
@@ -11,38 +12,95 @@ function updateModal(majors) {
   select(majors);
 } 
 
-
 initModal();
 var selected = [];
+
+// make const?
+var compareModal = {
+  modalId: "",
+  selectId: "",
+  openId: "",
+  deselId: "",
+  selId: "",
+};
+
+
+var factorsModal = {
+  modalId: "",
+  selectId: "",
+  openId: "",
+  deselId: "",
+  selId: "",
+};
+
 
 function initModal() {
   var fs = require("fs");
   var data = JSON.parse(fs.readFileSync("./src/data.json", "utf8"));
   var selector = document.getElementById("sel");
 
-  for (x in data["majors"]) {
-      option = document.createElement("option")
-      option.innerHTML = x + " (" + data["majors"][x]['Major_category'] + ")";
-      option.value = x;
+  var option;
+  var optgroup;
+
+  for (x in data["categories"]) {
+    optgroup = document.createElement("optgroup");
+    optgroup.label = x;
+    selector.append(optgroup);
+    for (y in data["categories"][x]) {
+
+      option = document.createElement("option");
+      option.innerHTML = data["categories"][x][y]["Major"];
+      option.value = data["categories"][x][y]["Major"];
 
       option.onclick = function() {
         option.selected = "selected";
       }
 
-      selector.append(option);
+      // fix ctrl problem
+      option.onmousedown = function(event) {
+        event.preventDefault();
+        var scroll_offset= this.parentElement.scrollTop;
+        this.selected= !this.selected;
+        this.parentElement.scrollTop= scroll_offset;
+      }
+      option.onmousemove = function(event) {
+          event.preventDefault();
+      }
+
+      optgroup.append(option);
+    }
   }
 
-  document.getElementById("sel-button").onclick = saveSelections;
-  document.getElementById("open-button").onclick = resetSelections;
+
+  document.getElementById("sel-button").addEventListener("click", function() {
+    saveSelections();
+  })
+
+  document.getElementById("open-button").addEventListener("click", function() {
+    resetSelections();
+  })
+
+  document.getElementById("desel-button").addEventListener("click", function() {
+    deselectAll();
+  })
+
+  option = document.createElement("option"); // quick fix for problem with last item always getting selected
+  optgroup = document.createElement("optgroup");
 }
 
 function resetSelections() {
-  select(selected);
+  select(selected, "sel");
+}
+
+function deselectAll() {
+  tmp = selected;
+  select([], "sel");
+  selected = tmp;
 }
 
 function saveSelections() {
   selected = [];
-  $("#sel > option").each(function() {
+  $("#sel > optgroup > option").each(function() {
       if (this.selected) {
         selected.push(this.value); 
       }
@@ -51,9 +109,9 @@ function saveSelections() {
 }
 
 // selects given majors in modal (input example: ["Aerospace Engineering", "Materials Science"]) 
-function select(majors) {
+function select(majors, selectID) {
   selected = [];
-  $("#sel > option").each(function() {
+  $("#" + selectID + " > optgroup > option").each(function() {
       this.selected = majors.includes(this.value);
       if (this.selected) {
         selected.push(this.value);
